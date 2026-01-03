a6b67c44d5dec1314f8083006e240b58da59b478
[dyad] <allyonasmedia@gmail.com>
2026-01-03 19:15:56 +0300

    [dyad] Re-added explicit React import to App.tsx - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /** @jsxImportSource react */
-// Removed: import React from "react";
+import React from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
